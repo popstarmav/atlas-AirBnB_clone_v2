4d653099c65e3642f387570a0f37e6f239519174
popstarmav <cj.johnson2017@gmail.com>
2024-06-29 12:09:19 -0500

    Mysql test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+# CREATE THE DATABASE
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+# CREATE THE USER
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+# GRANT ALL PRIVILEGES ON THE DATABASE TO THE USER
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+# GRANT SELECT ON performance_schema TO THE USER
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
