5bac66e7b9880fe5883b16878838c5837e52e908
UmbraServus <ksprague0592@gmail.com>
2024-06-30 12:37:46 -0500

    added the delete feature in filestorage; also updated all to be cls specific returning cls obj if specified or all objs if cls is None

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """returns the object dictionaries"""
+        if cls is None:
+            return self.__objects
+        
+        cls_dict = {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        return cls_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +28,15 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """ deletes object from __objects if it is in __objects"""
+        if obj is None:
+            return
+        if obj:
+            k = obj.__class__.__name__ + "." + obj.id
+        if k in self.__objects:
+            del self.__objects[k]
 
     def reload(self):
         """Loads storage dictionary from file"""
