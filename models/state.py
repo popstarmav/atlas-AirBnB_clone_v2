50be15eabc061b42d69807c70acc0285f0afedd1
popstarmav <cj.johnson2017@gmail.com>
2024-06-30 12:07:33 -0500

    Uodate inherits,attributes and relationship with class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """ State class, contains 'name' and relationship to City """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    
+    def cities(self):
+        """ Getter for cities """
+        if models.storage_t == 'db':
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
+        else:
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
