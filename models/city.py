d304f42df1d50e2615ed16d41ee9a2a168985b78
UmbraServus <ksprague0592@gmail.com>
2024-07-02 16:03:30 -0500

    added an except  for when the  json file is empty by initializing file with an empty dictionary in the in filestorage.reload

--- models/city.py
+++ models/city.py
@@ -3,14 +3,14 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-import models 
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if models.storage_type == 'db':
+    from models import storage_type
+    __tablename__ = 'cities'
     
-        __tablename__ = 'cities'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship('Place', back_populates='city', cascade='all, delete')
