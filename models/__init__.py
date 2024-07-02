46c55f8157d64644c21d26d24052b9c9764c3261
popstarmav <cj.johnson2017@gmail.com>
2024-07-02 23:23:32 +0000

    'if storage_type giving error

--- models/__init__.py
+++ models/__init__.py
@@ -4,7 +4,7 @@ import os
 
 storage_type = os.getenv('HBND_TYPE_STORAGE')
 
-if models.storage_type == 'db':
+if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
