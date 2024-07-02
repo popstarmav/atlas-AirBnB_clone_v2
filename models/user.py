a0dcf062b4d921de6f16aaf654ced3f27223ba91
popstarmav <cj.johnson2017@gmail.com>
2024-07-02 18:59:00 +0000

    Add place class attribute

--- models/user.py
+++ models/user.py
@@ -15,6 +15,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = relationship('Place', back_populates='user', cascade='all, delete')
     else:
         email = ""
         password = ""
