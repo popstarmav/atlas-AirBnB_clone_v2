32be05002026e7e95a738d5cff8ef350b8edfa47
UmbraServus <ksprague0592@gmail.com>
2024-07-02 17:07:09 -0500

    added if storage_type to state

--- models/state.py
+++ models/state.py
@@ -5,8 +5,13 @@ from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 class State(BaseModel, Base):
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan', passive_deletes=True)
+    import models
+    if models.storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all, delete-orphan', passive_deletes=True)
+    
+    else:
+        name = ""
 
     @property
     def cities(self):
