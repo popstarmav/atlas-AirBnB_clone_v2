65160d4ffe0e9768a2cd0f30c145f7dd6b490a6f
popstarmav <cj.johnson2017@gmail.com>
2024-07-02 18:55:57 +0000

    Add place class attribute

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 
@@ -10,3 +11,4 @@ class City(BaseModel):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', back_populates='city', cascade='all, delete')
