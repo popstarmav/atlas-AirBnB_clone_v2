630a440dbafa8b48049db3aa30083f0d698374a3
popstarmav <cj.johnson2017@gmail.com>
2024-07-02 01:29:43 +0000

    still fixing 6

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -41,7 +42,6 @@ class BaseModel():
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
