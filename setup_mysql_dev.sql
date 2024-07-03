91e5153a7fa569ca67e867b3406f68b146ae5a0c
popstarmav <cj.johnson2017>
2024-07-03 15:36:56 +0000

    fix comment in mysql_dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,17 @@
 -- Setup Db for dev
 
+-- Check if the database 'hbnb_dev_db' exists, and create it if it doesn't
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Check if the user 'hbnb_dev' exists on 'localhost', and create it with a password if it doesn't
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the database 'hbnb_dev_db' to the user 'hbnb_dev'
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant the user 'hbnb_dev' the privilege to perform SELECT operations on the 'performance_schema' database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply the changes made by the GRANT statements
 FLUSH PRIVILEGES;
+
