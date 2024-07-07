daca8d1d83d918db12646ec7b011085710cdba10
popstarmav <cj.johnson2017@gmail.com>
2024-07-07 15:59:40 +0000

    print Hello HBNB

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
+"""
+Simple flask app script
+App listen on '0.0.0.0'
+Port 5000 and defines one route
+"""
+
 from flask import Flask
 
+# Initialize the flask application
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
+
 def hello_hbnb():
+    """
+    Route Handler for the root URL ('/').
+    Returns str: Display "Hello HBNB!" on webpage.
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
+    # Run Flask app on host 0.0.0.0 and port 5000
     app.run(host='0.0.0.0', port=5000)
