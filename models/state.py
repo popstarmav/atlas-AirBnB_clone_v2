e19884c3f111366da3ccf0fbd953fee7747a91ba
popstarmav <cj.johnson@gmail.com>
2024-07-10 02:03:41 +0000

    update state method

--- models/state.py
+++ models/state.py
@@ -3,9 +3,12 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.city import City
+from models import storage_type
+import models
 
 
-class State(BaseModel, Base):
+class State(BaseModel, Base if storage_type == 'db' else object):
     from models import storage_type
     
     __tablename__ = 'states'
@@ -27,3 +30,15 @@ class State(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    
+    if storage_type != 'db':
+        @property
+        def cities(self):
+            """Return the list of City objects from storage linked to the current State."""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
