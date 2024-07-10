a1f40fffdef926182e7c1e249188f732d9779cbf
popstarmav <cj.johnson@gmail.com>
2024-07-10 02:06:08 +0000

    added public method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -62,4 +62,7 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+    
+    def close(self):
+        self.__session.remove()
 
